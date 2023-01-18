d550932cc8171ec1b3e3a153421eae9577f6f12b
Samuel S Rodrigues <samuelstievenrodrigues@gmail.com>
2023-01-18 20:54:53 -0300

    learning express and request

--- express/exercise.js
+++ express/exercise.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const app = express()
 var soma = require('../aula03/somar')
-var strSoma = String(soma(1, 1))
+var strSoma = String(soma(1, 2))
 
 app.get('/', function (req, res) {
   res.send(strSoma)
+})
+
+app.get('/ola/:nome/:cargo', function (req, res) {
+  res.send('Ola ' + req.params.nome)
+})
+
+app.get('/test/:cargo/:nome/:cidade', function (req, res) {
+  res.send(
+    '<h1> Ola, ' +
+      req.params.nome +
+      '!</h1>' +
+      '<h2> Seu cargo e: ' +
+      req.params.cargo +
+      ';</h2>' +
+      '<h3> Resido na cidade de: ' +
+      req.params.cidade +
+      '.'
+  )
 })
 
 app.listen(8081, function () {
